8bd34210603b7e2dd6bb2dd74f599538cc9d1125
OscarEduardoMartinezAcevedo <o.martineza@uniandes.edu.co>
2020-08-30 23:05:06 -0500

    Intento de nueva lista con datos relevantes: fallido

--- App/reto.py
+++ App/reto.py
@@ -87,11 +87,30 @@ def loadMovies ():
     print("Datos cargados, " + str(lt.size(SinD)) + " elementos cargados con "+str(lt.size(ConD))+" elementos complementarios")
     return [SinD,ConD]
 
-def infoall():
-    return
+def info(list):
+    lawiki=lt.newList()
+    for peliSD in loadMovies()[0]:
+        for peliCD in loadMovies()[1]:
+            if peliSD[1]==peliCD[1]:
+                pel={'id':peliSD[1],'average':peliSD[7],'count':peliSD[9],'genre':peliSD[2],
+                'director':peliCD[2],'actor1':[peliCD[1]],'actor2':peliCD[3],
+                'actor3':peliCD[5],'actor4':peliCD[7],'actor5':peliCD[9]}
+                lt.addLast(lawiki,pel)
+                
+    return lawiki
+                
 
-def mejorespelisD(director):
-    return
+def mejorespelisD(director,list):
+    best=lt.newList()
+    promedio=0
+    for eta in info(loadMovies):
+        if str(eta['director'])==str(director):
+            if eta['average']>=6:
+                lt.addLast(best,eta)
+            else:
+                promedio+=eta['average']
+    return ("Sus mejores pelis: "+str(best)+"\n Total de mejores pelis: "+str(len(best))+
+            "\n Promedio de las mejores: "+str(promedio/len(best)))
 
 
 def main():
@@ -112,9 +131,8 @@ def main():
             if int(inputs[0])==1: #Carga de pelis
                 lstmovies = loadMovies()
                 return lstmovies
-
             elif int(inputs[0])==2: #Mejores pelis de X
-                pass
+                return mejorespelisD(input("Quiero mirar las mejores pelis de "),info(loadMovies))
 
             elif int(inputs[0])==3: #Ranking de pelis personalizado
                 pass
@@ -129,7 +147,8 @@ def main():
                 pass
             elif int(inputs[0])==7:#Ranking de genero personalizado
                 pass
-
+            elif int(inputs[0]==9):
+                return info(loadMovies)
 
             elif int(inputs[0])==0: #opcion 0, salir
                 sys.exit(0)
